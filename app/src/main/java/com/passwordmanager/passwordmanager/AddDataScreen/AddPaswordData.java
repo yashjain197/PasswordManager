94c43d820c5bd26240d4848a3d51594a839b7b97
Yash Jain <yashjain19072001@gmail.com>
2022-02-24 21:45:38 +0000

    Committed

---
+++ app/src/main/java/com/passwordmanager/passwordmanager/AddDataScreen/AddPaswordData.java
@@ -0,0 +1,16 @@
+package com.passwordmanager.passwordmanager.AddDataScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.passwordmanager.passwordmanager.R;
+
+public class AddPaswordData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pasword_data);
+    }
+}
--- app/src/main/java/com/passwordmanager/passwordmanager/MainScreen/MainActivity.java
+++ app/src/main/java/com/passwordmanager/passwordmanager/MainScreen/MainActivity.java
@@ -2,15 +2,27 @@ package com.passwordmanager.passwordmanager.MainScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.passwordmanager.passwordmanager.AddDataScreen.AddPaswordData;
 import com.passwordmanager.passwordmanager.R;
+import com.passwordmanager.passwordmanager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityMainBinding binding;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Setting click listner on fab button
+        binding.addPassword.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, AddPaswordData.class));
+        });
+
     }
 }
